e51958c56b9baf80ddd45313c68b5ca64b867ae8
Carlos Vargas <cavargasp@intec.co>
2021-10-01 14:59:43 -0500

    fix: run lint and unit test

--- app/controllers/users.ts
+++ app/controllers/users.ts
@@ -5,22 +5,22 @@ import userService from '../services/users';
 import { User } from '../models/user';
 import { notFoundError } from '../errors';
 
-export function getUsers(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-  return userService
+export function getUsers(req: Request, res: Response, next: NextFunction): void {
+  userService
     .findAll()
     .then((users: User[]) => res.send(users))
     .catch(next);
 }
 
-export function createUser(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-  return userService
+export function createUser(req: Request, res: Response, next: NextFunction): void {
+  userService
     .createAndSave({ username: req.body.username } as User)
     .then((user: User) => res.status(HttpStatus.CREATED).send({ user }))
     .catch(next);
 }
 
-export function getUserById(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-  return userService
+export function getUserById(req: Request, res: Response, next: NextFunction): void {
+  userService
     .findUser({ id: parseInt(req.params.id) })
     .then((user: User) => {
       if (!user) {
