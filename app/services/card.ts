9737ba7a9dda953b26d08aed6f7b21e5d6d221ad
Carlos Vargas <cavargasp@intec.co>
2021-10-06 09:58:33 -0500

    fix: move interface use function over class

--- app/services/card.ts
+++ app/services/card.ts
@@ -1,33 +1,30 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { CardInfo, Cards } from '../../types/app/card';
 
-class Card {
-  options: AxiosRequestConfig = {
-    method: 'GET',
-    url: 'info',
-    headers: {
-      'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
-      'x-rapidapi-key': process.env.RAPIDAPI_KEY
-    }
-  };
-
-  info(): Promise<CardInfo> {
-    return this.genericQuery('info') as Promise<CardInfo>;
+const genericOptions: AxiosRequestConfig = {
+  method: 'GET',
+  url: 'info',
+  headers: {
+    'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
+    'x-rapidapi-key': process.env.RAPIDAPI_KEY
   }
+};
 
-  cards(): Promise<Cards> {
-    return this.genericQuery('allCards') as Promise<Cards>;
-  }
-
-  async genericQuery(endPoint: string): Promise<unknown> {
-    const options = { ...this.options };
-    options.url = this.getUrl(endPoint);
-    const result = await axios.request(options);
-    return result.data;
-  }
-
-  private getUrl(endPorint: string): string {
-    return `https://omgvamp-hearthstone-v1.p.rapidapi.com/${endPorint}`;
-  }
+function getUrl(endPorint: string): string {
+  return `https://omgvamp-hearthstone-v1.p.rapidapi.com/${endPorint}`;
 }
-export const card = new Card();
+
+async function genericQuery(endPoint: string): Promise<unknown> {
+  const options = { ...genericOptions };
+  options.url = getUrl(endPoint);
+  const result = await axios.request(options);
+  return result.data;
+}
+
+export function info(): Promise<CardInfo> {
+  return genericQuery('info') as Promise<CardInfo>;
+}
+
+export function cards(): Promise<Cards> {
+  return genericQuery('allCards') as Promise<Cards>;
+}
