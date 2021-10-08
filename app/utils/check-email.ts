98f10e50c6962ecc2c56268b83cf43086be6a3ea
Carlos Vargas <cavargasp@intec.co>
2021-10-08 17:30:37 -0500

    fix: clear unused function

--- app/utils/check-email.ts
+++ app/utils/check-email.ts
@@ -1,16 +1,8 @@
 import { InternalError } from '../middlewares/error-handler';
 import userService from '../services/users';
-import { conflictRequest, unprocessableEntity } from '../errors';
-
-export function checkEmailDomain(email: string): boolean {
-  const regexpEmail = /@wolox\.co$/;
-  return regexpEmail.test(email);
-}
+import { conflictRequest } from '../errors';
 
 export async function checkEmail(email: string): Promise<InternalError | null> {
-  if (!checkEmailDomain(email)) {
-    return unprocessableEntity('email does not belong to domain');
-  }
   const user = await userService.findUser({ email });
   if (user) {
     return conflictRequest('the email is taken');
