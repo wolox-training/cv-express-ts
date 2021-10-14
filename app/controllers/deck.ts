302fa874bf70c5d2f23b82849d3efd23d42a4fc0
Carlos Vargas <cavargasp@intec.co>
2021-10-14 15:43:24 -0500

    feat: check name of deck

--- app/controllers/deck.ts
+++ app/controllers/deck.ts
@@ -4,9 +4,15 @@ import { alreadyExistError, databaseError } from '../errors';
 import { UserDeck } from '../models/user-deck';
 import { createAndSave, findUserDeck } from '../services/user-deck';
 import { HTTP_CODES } from '../constants';
+import { CARD_SET } from '../constants/card-set';
 
 export async function createDeck(req: Request, res: Response, next: NextFunction): Promise<void> {
   const cardSet = req.body.deckName;
+  const cardSetExists = CARD_SET.some((name: string) => cardSet === name);
+  if (!cardSetExists) {
+    next(alreadyExistError);
+    return;
+  }
   const userId = req.user.id;
   const userDeck = {
     userId,
@@ -18,8 +24,8 @@ export async function createDeck(req: Request, res: Response, next: NextFunction
       next(alreadyExistError('The user already has the deck'));
       return;
     }
-    await createAndSave(userDeck as UserDeck);
-    res.status(HTTP_CODES.CREATED).send();
+    const deck = await createAndSave(userDeck as UserDeck);
+    res.status(HTTP_CODES.CREATED).send({ id: deck.id });
   } catch (e) {
     next(databaseError(''));
   }
